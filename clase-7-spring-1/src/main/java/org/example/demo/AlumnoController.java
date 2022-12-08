c379135544fe53a5afc01cab0d4665b722887f39
zphoenixz <rora.vvaa@gmail.com>
2022-12-07 21:22:17 -0400

    clase 7

--- clase-7-spring-1/src/main/java/org/example/demo/AlumnoController.java
+++ clase-7-spring-1/src/main/java/org/example/demo/AlumnoController.java
@@ -13,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
+    @GetMapping(value = "hola")
+    public ResponseEntity<?> holaMund() {
+        return ResponseEntity.ok("Hola Mundo");
+    }
     // CRUD ###################################
+
+
     //CREATE -------------------------
+    //http://localhost:8080/alumno/crear
     @GetMapping(value = "alumno/crear")
     public ResponseEntity<?> crearAlumno() {
         Alumno nuevoAlumno = new Alumno(
@@ -28,17 +35,20 @@ public class AlumnoController {
     }
 
     //READ -------------------------
+    //http://localhost:8080/alumno/leer/12345
     @GetMapping(value = "alumno/leer/{dni}")
     public ResponseEntity<?> obtenerAlumnosPorDNI(@PathVariable(name = "dni") final Long dni) {
         return ResponseEntity.ok(alumnoRepository.getReferenceById(dni));
     }
-
+    //http://localhost:8080/alumno/leer/todos
     @GetMapping(value = "alumno/leer/todos")
     public ResponseEntity<?> obtenerTodosLosAlumnos() {
         return ResponseEntity.ok(alumnoRepository.findAll());
     }
 
-    @GetMapping(value = "alumno/leer/{dni}")
+    //READ -------------------------
+    //http://localhost:8080/alumno/actualizar/12345
+    @GetMapping(value = "alumno/actualizar/{dni}")
     public ResponseEntity<?> actualizarNombrePorDNI(@PathVariable(name = "dni") final Long dni) {
         Alumno alumno = alumnoRepository.getReferenceById(dni);
         alumno.setNombre("NombreNuevo");
